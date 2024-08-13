9a29996421c4150e7c17aebf1c5b93b0e8dc945e
Helin Wang <49939413+WangHelin1997@users.noreply.github.com>
2024-08-13 16:29:35 -0700

    Update inference_scale.py

--- inference_scale.py
+++ inference_scale.py
@@ -16,7 +16,7 @@ from data.tokenizer import (
 import time
 
 @torch.no_grad()
-def inference_one_sample(model, model_args, phn2num, text_tokenizer, audio_tokenizer, audio_fn, prompt_text, target_text, mask_interval, cfg_coef, aug_text, aug_context, cfg_pretrained, use_watermark, device, decode_config):
+def inference_one_sample(model, model_args, phn2num, text_tokenizer, audio_tokenizer, audio_fn, prompt_text, target_text, mask_interval, cfg_coef, aug_text, aug_context, cfg_pretrained, use_watermark, tts, device, decode_config):
     # phonemize
     text_tokens = [phn2num[phn] for phn in
             tokenize_text(
@@ -34,8 +34,8 @@ def inference_one_sample(model, model_args, phn2num, text_tokenizer, audio_token
     prompt_text_tokens = torch.LongTensor(prompt_text_tokens).unsqueeze(0)
     prompt_text_tokens_lens = torch.LongTensor([prompt_text_tokens.shape[-1]])
 
-    encoded_frames = tokenize_audio(audio_tokenizer, audio_fn)
-    original_audio = encoded_frames[0][0].transpose(2,1) # [1,T,K]
+    encoded_frames, scale, emb = tokenize_audio(audio_tokenizer, audio_fn)
+    original_audio = encoded_frames.transpose(2,1) # [1,T,K]
     assert original_audio.ndim==3 and original_audio.shape[0] == 1 and original_audio.shape[2] == model_args.n_codebooks, original_audio.shape
     logging.info(f"with direct encodec encoding before input, original audio length: {original_audio.shape[1]} codec frames, which is {original_audio.shape[1]/decode_config['codec_sr']:.2f} sec.")
 
@@ -68,20 +68,21 @@ def inference_one_sample(model, model_args, phn2num, text_tokenizer, audio_token
     # decode 
     if use_watermark:
         wav, sr = torchaudio.load(audio_fn)
-        new_wav = torch.zeros(1, encoded_frames.shape[-1]*320) # codec hz
+        new_emb = torch.zeros((1, emb.shape[1], encoded_frames.shape[-1])).to(encoded_frames.device)
         
         ori_non_mask_intervals = [(max(item[0],0), item[1]) for item in ori_masks]
         non_mask_intervals = [(max(item[0],0), item[1]) for item in masks]
-
         for i in range(len(ori_non_mask_intervals)):
-            
-            tmp_wav = wav[:, ori_non_mask_intervals[i][0]*320:ori_non_mask_intervals[i][1]*320]
-            leng = min(non_mask_intervals[i][1]*320-non_mask_intervals[i][0]*320, tmp_wav.shape[-1])
-            new_wav[:, non_mask_intervals[i][0]*320:non_mask_intervals[i][0]*320+leng] = tmp_wav
-        print(marks)
-        generated_sample = audio_tokenizer.wmdecode(encoded_frames, marks.to(encoded_frames.device), new_wav.unsqueeze(0).to(encoded_frames.device))
+            new_emb[..., non_mask_intervals[i][0]:non_mask_intervals[i][1]] = emb[..., ori_non_mask_intervals[i][0]:ori_non_mask_intervals[i][1]]
+
+        generated_sample = audio_tokenizer.wmdecode(encoded_frames, marks.to(encoded_frames.device), new_emb, scale)
     else:
-        generated_sample = audio_tokenizer.decode(encoded_frames)
+        raise RuntimeError("SSR-Speech must add watermarks!")
+
+    if tts:
+        wav, sr = torchaudio.load(audio_fn)
+        generated_sample = generated_sample[:,:, wav.shape[-1]-320:]
+        
     return generated_sample
 
 
@@ -90,7 +91,6 @@ def get_mask_interval(ali_fn, word_span):
         data = [l.strip().split(",") for l in rf.readlines()]
         data = data[1:]
     data = [item for item in data if item[3] == 'words']
-    # print(data)
     s, e = word_span[0], word_span[1]
     assert s <= e, f"s:{s}, e:{e}"
     assert s >= 0, f"s:{s}"
